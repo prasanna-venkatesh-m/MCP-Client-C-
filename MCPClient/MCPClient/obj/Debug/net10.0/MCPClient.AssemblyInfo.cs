417ff4772f114991ac4a1a728b69a756f07d8ba3
Prasanna <prasannavenkatesh.m@arisecapital.in>
2026-03-22 20:17:04 +0530

    Clean commit

--- MCPClient/MCPClient/obj/Debug/net10.0/MCPClient.AssemblyInfo.cs
+++ MCPClient/MCPClient/obj/Debug/net10.0/MCPClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MCPClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a749f004e7d0610a0c4122d6b688e954f3fa6c43")]
 [assembly: System.Reflection.AssemblyProductAttribute("MCPClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MCPClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
